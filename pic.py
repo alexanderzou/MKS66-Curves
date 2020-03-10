8bae689b274da31ba36fbfdf8490ab45eb666ac1
alexanderzou <azou00stuy.edu>
2020-03-10 10:52:24 -0400

    pic/gif made

--- pic.py
+++ pic.py
@@ -8,12 +8,20 @@ def numConv(num):
         return new
 
 f = open('snake', 'w')
-fNum = 1
 a = -1000
-while a < 1000:
+while a < 500:
     f.write('bezier\n200 {} 100 {} 300 {} 200 {}\n'.format(a, a+200, a+300, a+500))
     f.write('bezier\n300 {} 200 {} 400 {} 300 {}\n'.format(a, a+200, a+300, a+500))
-    f.write('circle\n')
+    f.write('circle\n200 {} 0 20\n'.format(a+100))
+    f.write('circle\n250 {} 0 10\n'.format(a+300))
+    f.write('circle\n260 {} 0 30\n'.format(a+400))
+    f.write('circle\n240 {} 0 15\n'.format(a+180))
     a += 500
+f.write('ident\nmove\n0 5 0\n')
+fNum = 1
+tmp = fNum
+while fNum < tmp + 100:
+    f.write('apply\nsave\nimage{}.png\n'.format(numConv(fNum)))
+    fNum += 1
 f.write('display\n')
 f.close()
